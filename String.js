4340559753a33b9a2f8f09893e55532947a70726
Raja <kraj9380286@gmail.com>
2026-01-20 10:55:05 +0530

    commited

--- String.js
+++ String.js
@@ -1,5 +1,15 @@
 // let str="RajaKUMAR";
 // console.log(str[5]);
 
-let str='Rahul KUmar';
-console.log("str=",str);
+// let str='Rahul KUmar';
+// console.log("str=",str);
+ 
+/*  using templete  Literals*/
+  //TL is used to single line output 
+  let obj={
+    Name:"raja",
+    branch:"CSIT",
+    section: "II"
+  }
+  console.log(obj.Name);
+
