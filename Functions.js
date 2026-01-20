940c4095ec77187ed03e50646361e61ffb0eabcb
Raja <kraj9380286@gmail.com>
2026-01-20 20:11:43 +0530

    commited

--- Functions.js
+++ Functions.js
@@ -1,5 +1,12 @@
-function myfunction(){
-    console.log("Welcome");
-    console.log("Hello");
-}
- myfunction();
+// function myfunction(){
+//     console.log("Welcome");
+//     console.log("Hello");
+// }
+//  myfunction();
+
+/* Arrow Function*/
+ const Arrsum=(a,b)=>{
+    console.log(a+b);
+ };
+
+ Arrsum(7,3);
