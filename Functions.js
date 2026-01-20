1d23d8c44c5f9d90c181e9a3ae04236cd8568930
Raja <kraj9380286@gmail.com>
2026-01-20 20:26:30 +0530

    commited

--- Functions.js
+++ Functions.js
@@ -11,6 +11,24 @@
 
 //  Arrsum(7,3);
 
- const printHell0=()=>{
-  console.log("Hello friends");
- }
+//  const printHell0=()=>{
+//   console.log("Hello friends");
+//  }
+
+      /*1.Practice Question*/  // Hello ->vowels->2
+      
+  function countVowel(val) {
+    let count = 0;
+    let vowel = ['A','E','I','O','U','a','e','i','o','u'];
+
+    for (let i of val) {
+        for (let j of vowel) {
+            if (i === j) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+console.log(countVowel("hello")); // 2
